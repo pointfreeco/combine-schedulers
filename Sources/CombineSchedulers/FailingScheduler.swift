0c3e651ed9b490347a91a26103a6ae3963212bb6
Stephen Celis <stephen@stephencelis.com>
2021-03-16 21:46:57 -0400

    wip

--- Sources/CombineSchedulers/FailingScheduler.swift
+++ Sources/CombineSchedulers/FailingScheduler.swift
@@ -49,6 +49,7 @@
     }
   }
 
+  @available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
   extension Scheduler
   where
     SchedulerTimeType == DispatchQueue.SchedulerTimeType,
@@ -67,6 +68,7 @@
     }
   }
 
+  @available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
   extension Scheduler
   where
     SchedulerTimeType == RunLoop.SchedulerTimeType,
