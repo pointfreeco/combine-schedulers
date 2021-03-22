d6af6cac375e4163395e6b2a6a82dad500347f91
stephencelis <stephencelis@users.noreply.github.com>
2021-03-22 14:25:48 +0000

    Run swift-format

--- Package.swift
+++ Package.swift
@@ -17,19 +17,19 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.1.0"),
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.1.0")
   ],
   targets: [
     .target(
       name: "CombineSchedulers",
       dependencies: [
-        "XCTestDynamicOverlay",
+        "XCTestDynamicOverlay"
       ]
     ),
     .testTarget(
       name: "CombineSchedulersTests",
       dependencies: [
-        "CombineSchedulers",
+        "CombineSchedulers"
       ]
     ),
   ]
