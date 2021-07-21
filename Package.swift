4e5cbe12b87450265642680f4d9e8b5332b5df8a
Maxim Krouk <40476363+maximkrouk@users.noreply.github.com>
2021-07-21 18:04:08 +0400

    feat: Remove platform bindings (#33)

--- Package.swift
+++ Package.swift
@@ -4,12 +4,6 @@ import PackageDescription
 
 let package = Package(
   name: "combine-schedulers",
-  platforms: [
-    .iOS(.v10),
-    .macOS(.v10_12),
-    .tvOS(.v10),
-    .watchOS(.v3),
-  ],
   products: [
     .library(
       name: "CombineSchedulers",
