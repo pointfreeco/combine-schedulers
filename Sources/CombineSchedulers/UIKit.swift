3e9f494bbe1829290928c28c5d65ce47d4d50f36
Stephen Celis <stephen@stephencelis.com>
2021-08-25 13:50:08 -0400

    wip

--- Sources/CombineSchedulers/UIKit.swift
+++ Sources/CombineSchedulers/UIKit.swift
@@ -1,108 +1,110 @@
-import Combine
-import SwiftUI
+#if canImport(UIKit)
+  import Combine
+  import UIKit
 
-extension Scheduler {
-  /// Wraps scheduled actions in `UIView.animate`.
-  ///
-  /// - Parameter duration: The `duration` parameter passed to `UIView.animate`.
-  /// - Parameter delay: The `delay` parameter passed to `UIView.animate`.
-  /// - Parameter animationOptions: The `options` parameter passed to `UIView.animate`
-  /// - Returns: A scheduler that wraps scheduled actions in `UIView.animate`.
-  public func animate(
-    withDuration duration: TimeInterval,
-    delay: TimeInterval = 0,
-    options animationOptions: UIView.AnimationOptions = []
-  ) -> AnySchedulerOf<Self> {
-    AnyScheduler(
-      minimumTolerance: { self.minimumTolerance },
-      now: { self.now },
-      scheduleImmediately: { options, action in
-        self.schedule(options: options) {
-          UIView.animate(
-            withDuration: duration,
-            delay: delay,
-            options: animationOptions,
-            animations: action
-          )
+  extension Scheduler {
+    /// Wraps scheduled actions in `UIView.animate`.
+    ///
+    /// - Parameter duration: The `duration` parameter passed to `UIView.animate`.
+    /// - Parameter delay: The `delay` parameter passed to `UIView.animate`.
+    /// - Parameter animationOptions: The `options` parameter passed to `UIView.animate`
+    /// - Returns: A scheduler that wraps scheduled actions in `UIView.animate`.
+    public func animate(
+      withDuration duration: TimeInterval,
+      delay: TimeInterval = 0,
+      options animationOptions: UIView.AnimationOptions = []
+    ) -> AnySchedulerOf<Self> {
+      AnyScheduler(
+        minimumTolerance: { self.minimumTolerance },
+        now: { self.now },
+        scheduleImmediately: { options, action in
+          self.schedule(options: options) {
+            UIView.animate(
+              withDuration: duration,
+              delay: delay,
+              options: animationOptions,
+              animations: action
+            )
+          }
+        },
+        delayed: { date, tolerance, options, action in
+          self.schedule(after: date, tolerance: tolerance, options: options) {
+            UIView.animate(
+              withDuration: duration,
+              delay: delay,
+              options: animationOptions,
+              animations: action
+            )
+          }
+        },
+        interval: { date, interval, tolerance, options, action in
+          self.schedule(after: date, interval: interval, tolerance: tolerance, options: options) {
+            UIView.animate(
+              withDuration: duration,
+              delay: delay,
+              options: animationOptions,
+              animations: action
+            )
+          }
         }
-      },
-      delayed: { date, tolerance, options, action in
-        self.schedule(after: date, tolerance: tolerance, options: options) {
-          UIView.animate(
-            withDuration: duration,
-            delay: delay,
-            options: animationOptions,
-            animations: action
-          )
-        }
-      },
-      interval: { date, interval, tolerance, options, action in
-        self.schedule(after: date, interval: interval, tolerance: tolerance, options: options) {
-          UIView.animate(
-            withDuration: duration,
-            delay: delay,
-            options: animationOptions,
-            animations: action
-          )
-        }
-      }
-    )
-  }
+      )
+    }
 
-  /// Wraps scheduled actions in `UIView.animate`.
-  ///
-  /// - Parameter duration: The `duration` parameter passed to `UIView.animate`.
-  /// - Parameter delay: The `delay` parameter passed to `UIView.animate`.
-  /// - Parameter dampingRatio: The `dampingRatio` parameter passed to `UIView.animate`
-  /// - Parameter velocity: The `velocity` parameter passed to `UIView.animate`
-  /// - Parameter animationOptions: The `options` parameter passed to `UIView.animate`
-  /// - Returns: A scheduler that wraps scheduled actions in `UIView.animate`.
-  public func animate(
-    withDuration duration: TimeInterval,
-    delay: TimeInterval = 0,
-    usingSpringWithDamping dampingRatio: CGFloat,
-    initialSpringVelocity velocity: CGFloat,
-    options animationOptions: UIView.AnimationOptions
-  ) -> AnySchedulerOf<Self> {
-    AnyScheduler(
-      minimumTolerance: { self.minimumTolerance },
-      now: { self.now },
-      scheduleImmediately: { options, action in
-        self.schedule(options: options) {
-          UIView.animate(
-            withDuration: duration,
-            delay: delay,
-            usingSpringWithDamping: dampingRatio,
-            initialSpringVelocity: velocity,
-            options: animationOptions,
-            animations: action
-          )
+    /// Wraps scheduled actions in `UIView.animate`.
+    ///
+    /// - Parameter duration: The `duration` parameter passed to `UIView.animate`.
+    /// - Parameter delay: The `delay` parameter passed to `UIView.animate`.
+    /// - Parameter dampingRatio: The `dampingRatio` parameter passed to `UIView.animate`
+    /// - Parameter velocity: The `velocity` parameter passed to `UIView.animate`
+    /// - Parameter animationOptions: The `options` parameter passed to `UIView.animate`
+    /// - Returns: A scheduler that wraps scheduled actions in `UIView.animate`.
+    public func animate(
+      withDuration duration: TimeInterval,
+      delay: TimeInterval = 0,
+      usingSpringWithDamping dampingRatio: CGFloat,
+      initialSpringVelocity velocity: CGFloat,
+      options animationOptions: UIView.AnimationOptions
+    ) -> AnySchedulerOf<Self> {
+      AnyScheduler(
+        minimumTolerance: { self.minimumTolerance },
+        now: { self.now },
+        scheduleImmediately: { options, action in
+          self.schedule(options: options) {
+            UIView.animate(
+              withDuration: duration,
+              delay: delay,
+              usingSpringWithDamping: dampingRatio,
+              initialSpringVelocity: velocity,
+              options: animationOptions,
+              animations: action
+            )
+          }
+        },
+        delayed: { date, tolerance, options, action in
+          self.schedule(after: date, tolerance: tolerance, options: options) {
+            UIView.animate(
+              withDuration: duration,
+              delay: delay,
+              usingSpringWithDamping: dampingRatio,
+              initialSpringVelocity: velocity,
+              options: animationOptions,
+              animations: action
+            )
+          }
+        },
+        interval: { date, interval, tolerance, options, action in
+          self.schedule(after: date, interval: interval, tolerance: tolerance, options: options) {
+            UIView.animate(
+              withDuration: duration,
+              delay: delay,
+              usingSpringWithDamping: dampingRatio,
+              initialSpringVelocity: velocity,
+              options: animationOptions,
+              animations: action
+            )
+          }
         }
-      },
-      delayed: { date, tolerance, options, action in
-        self.schedule(after: date, tolerance: tolerance, options: options) {
-          UIView.animate(
-            withDuration: duration,
-            delay: delay,
-            usingSpringWithDamping: dampingRatio,
-            initialSpringVelocity: velocity,
-            options: animationOptions,
-            animations: action
-          )
-        }
-      },
-      interval: { date, interval, tolerance, options, action in
-        self.schedule(after: date, interval: interval, tolerance: tolerance, options: options) {
-          UIView.animate(
-            withDuration: duration,
-            delay: delay,
-            usingSpringWithDamping: dampingRatio,
-            initialSpringVelocity: velocity,
-            options: animationOptions,
-            animations: action
-          )
-        }
-      }
-    )
+      )
+    }
   }
-}
+#endif
