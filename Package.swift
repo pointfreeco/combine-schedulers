4cf088c29a20f52be0f2ca54992b492c54e0076b
Marius Landwehr <marius.landwehr@gmail.com>
2021-09-21 16:34:02 +0200

    Updated xctest-dynamic-overlay to 0.2.1 (#42)
    * Updated xctest-dynamic-overlay to 0.2.1
    
    * Package Resolved should not be in a Package at all

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.1.0")
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.2.1")
   ],
   targets: [
     .target(
