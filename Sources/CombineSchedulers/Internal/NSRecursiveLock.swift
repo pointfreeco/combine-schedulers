fa11983df77bebd283e7de3a11292e6ddc94f55f
Stephen Celis <stephen@stephencelis.com>
2022-07-18 13:46:08 -0400

    wip

--- Sources/CombineSchedulers/Internal/NSRecursiveLock.swift
+++ Sources/CombineSchedulers/Internal/NSRecursiveLock.swift
@@ -8,12 +8,3 @@ extension NSRecursiveLock {
     return operation()
   }
 }
-
-extension NSRecursiveLock {
-  @inlinable @discardableResult
-  func sync<R>(operation: () async -> R) async -> R {
-    self.lock()
-    defer { self.unlock() }
-    return await operation()
-  }
-}
