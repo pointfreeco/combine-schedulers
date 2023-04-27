9f496dbd1a38c1bae0b19d5c64a44f6ecf2d8b20
Stephen Celis <stephen@stephencelis.com>
2023-04-27 10:07:36 -0700

    Make `megaYield` configurable by env var

--- Sources/CombineSchedulers/TestScheduler.swift
+++ Sources/CombineSchedulers/TestScheduler.swift
@@ -285,10 +285,20 @@
   > where Scheduler: Combine.Scheduler
 
   extension Task where Success == Failure, Failure == Never {
-    static func megaYield(count: Int = 10) async {
-      for _ in 1...count {
+    // NB: We would love if this was not necessary. See this forum post for more information:
+    //     https://forums.swift.org/t/reliably-testing-code-that-adopts-swift-concurrency/57304
+    static func megaYield(count: Int = defaultMegaYieldCount) async {
+      for _ in 0..<count {
         await Task<Void, Never>.detached(priority: .background) { await Task.yield() }.value
       }
     }
   }
+
+  let defaultMegaYieldCount = max(
+    0,
+    min(
+      ProcessInfo.processInfo.environment["TASK_MEGA_YIELD_COUNT"].flatMap(Int.init) ?? 20,
+      10_000
+    )
+  )
 #endif
