7f14f2cfee99ee2d7a8eeaf002210a9a3cfadf23
stephencelis <stephencelis@users.noreply.github.com>
2023-07-17 18:21:10 +0000

    Run swift-format

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "0.1.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.8.5")
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.8.5"),
   ],
   targets: [
     .target(
