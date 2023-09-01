ab009de33025cb37b624e5c64f4e102236b967e4
mbrandonw <mbrandonw@users.noreply.github.com>
2023-09-01 18:23:17 +0000

    Run swift-format

--- Tests/CombineSchedulersTests/UnimplementedSchedulerTests.swift
+++ Tests/CombineSchedulersTests/UnimplementedSchedulerTests.swift
@@ -1,37 +1,38 @@
 #if compiler(>=5.4)
-#if canImport(Combine)
-  import Combine
-  import CombineSchedulers
-  import XCTest
+  #if canImport(Combine)
+    import Combine
+    import CombineSchedulers
+    import XCTest
 
-  final class UnimplementedSchedulerTests: XCTestCase {
-    func testFailure() {
-      let scheduler = RunLoop.unimplemented
+    final class UnimplementedSchedulerTests: XCTestCase {
+      func testFailure() {
+        let scheduler = RunLoop.unimplemented
 
-      XCTExpectFailure { _ = scheduler.now }
-      XCTExpectFailure { _ = scheduler.minimumTolerance }
-      XCTExpectFailure {
-        let expectation = self.expectation(description: "schedule")
-        scheduler.schedule(options: nil) {
-          expectation.fulfill()
+        XCTExpectFailure { _ = scheduler.now }
+        XCTExpectFailure { _ = scheduler.minimumTolerance }
+        XCTExpectFailure {
+          let expectation = self.expectation(description: "schedule")
+          scheduler.schedule(options: nil) {
+            expectation.fulfill()
+          }
+          self.wait(for: [expectation], timeout: 0.1)
         }
-        self.wait(for: [expectation], timeout: 0.1)
-      }
-      XCTExpectFailure {
-        let expectation = self.expectation(description: "schedule")
-        scheduler.schedule(after: .init(.init()), tolerance: .zero, options: nil) {
-          expectation.fulfill()
+        XCTExpectFailure {
+          let expectation = self.expectation(description: "schedule")
+          scheduler.schedule(after: .init(.init()), tolerance: .zero, options: nil) {
+            expectation.fulfill()
+          }
+          self.wait(for: [expectation], timeout: 0.1)
         }
-        self.wait(for: [expectation], timeout: 0.1)
-      }
-      XCTExpectFailure {
-        let expectation = self.expectation(description: "schedule")
-        _ = scheduler.schedule(after: .init(.init()), interval: 1, tolerance: .zero, options: nil) {
-          expectation.fulfill()
+        XCTExpectFailure {
+          let expectation = self.expectation(description: "schedule")
+          _ = scheduler.schedule(after: .init(.init()), interval: 1, tolerance: .zero, options: nil)
+          {
+            expectation.fulfill()
+          }
+          self.wait(for: [expectation], timeout: 0.1)
         }
-        self.wait(for: [expectation], timeout: 0.1)
       }
     }
-  }
-#endif // canImport(Combine)
+  #endif  // canImport(Combine)
 #endif
